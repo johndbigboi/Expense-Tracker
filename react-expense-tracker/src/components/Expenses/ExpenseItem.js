c4e38c78f890b1077ead51b571c8b5a3a8fa744d
john <jericcarreon@yahoo.com>
2022-10-31 20:53:46 +0000

    Adding, Import useState in ExpenseItem.js

--- react-expense-tracker/src/components/Expenses/ExpenseItem.js
+++ react-expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,21 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle('Updated!!!');
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
+        <button onClick={clickHandler}>Change Title</button>
+
         <div className="expense-item__price">{props.amount}</div>
       </div>
     </Card>
